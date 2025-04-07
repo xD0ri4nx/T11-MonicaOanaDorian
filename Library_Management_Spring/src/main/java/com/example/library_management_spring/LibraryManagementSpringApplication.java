191b16c56992902b48e3b6ef96afc7014828a655
Ruieneanu Dorian <133339065+xD0ri4nx@users.noreply.github.com>
2025-04-07 21:22:44 +0300

    Add files via upload

---
+++ Library_Management_Spring/src/main/java/com/example/library_management_spring/LibraryManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.library_management_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSpringApplication.class, args);
+    }
+
+}
---
+++ Library_Management_Spring/src/test/java/com/example/library_management_spring/LibraryManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_management_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
